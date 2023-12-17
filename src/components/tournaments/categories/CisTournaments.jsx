8dd0a69a22e94106a23cda1190128a103a3ebfce
totJOKER <117646766+totJOKER@users.noreply.github.com>
2023-12-17 21:07:35 +0700

    Update CisTournaments.jsx

--- src/components/tournaments/categories/CisTournaments.jsx
+++ src/components/tournaments/categories/CisTournaments.jsx
@@ -51,7 +51,7 @@ export default function CisTournaments() {
                                 <h3 className="tour-info-other fw400">Guarantee - {data.guarantee}</h3>
                                 <h3 className="tour-info-other fw400">Invitations - {data.invite ? 'yes' : 'none'}</h3>
                                 <div className="tour-button-box">
-                                    <a href={data.link} title={data.link}>Discord</a>
+                                    <a href={data.link} title={data.link}>Link</a>
                                 </div>
                             </div>
                         </li>
