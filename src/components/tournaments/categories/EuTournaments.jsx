fb8168186ddc6176d974320d04d44f344a3f8f82
totJOKER <117646766+totJOKER@users.noreply.github.com>
2023-12-18 13:56:05 +0700

    Update EuTournaments.jsx

--- src/components/tournaments/categories/EuTournaments.jsx
+++ src/components/tournaments/categories/EuTournaments.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react'
 import axios from "axios";
-import moment from "moment/moment";
 
-export default function EuTournaments() {
+export default function AllTournaments() {
 
     let [tour, setTour] = React.useState([])
     React.useEffect(() => {
@@ -12,11 +11,28 @@ export default function EuTournaments() {
             })
     }, [])
 
+    let [curr, setCurr] = React.useState([])
+    React.useEffect(() => {
+        let data = axios.get('https://www.cbr-xml-daily.ru/latest.js')
+            .then(res => {
+                setCurr(res.data);
+            })
+    }, [])
+
+    
     let currentTour = [];
     tour.map(data => {
-        // let checkStatus = data.
         currentTour.push({ id: data.id, name: data.name, orgName: data.orgName, teams: data.teams, prizepool: data.prizepool, guarantee: data.guarantee, openReg: data.openReg, link: data.link, image: data.image, currency: data.currency, invite: data.invite, regionCis: data.regionCis, regionEu: data.regionEu, closeReg: data.closeReg })
     })
+    let ratesTour = [];
+    currentTour.map(data => {
+        if (data.currency == 'rub') {
+            ratesTour.push({ id: data.id, name: data.name, orgName: data.orgName, teams: data.teams, prizepool: Math.floor(+data.prizepool * +curr.rates.USD), guarantee: data.guarantee, openReg: data.openReg, link: data.link, image: data.image, currency: 'usd', invite: data.invite, regionCis: data.regionCis, regionEu: data.regionEu, closeReg: data.closeReg })
+        } else {
+            ratesTour.push({ id: data.id, name: data.name, orgName: data.orgName, teams: data.teams, prizepool: data.prizepool, guarantee: data.guarantee, openReg: data.openReg, link: data.link, image: data.image, currency: data.currency, invite: data.invite, regionCis: data.regionCis, regionEu: data.regionEu, closeReg: data.closeReg })
+        }
+    })
+    console.log(ratesTour)
 
 
     const date = new Date();
@@ -24,9 +40,10 @@ export default function EuTournaments() {
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const nowDate = `${year}.${month}.${day}`;
+
     return (
         <ol className="tour-list">
-            {currentTour.map(data => {
+            {ratesTour.map(data => (
                 if (data.regionEu == true) {
                     return (
                         <li className="tour-card show" id={data.regionCis}>
@@ -50,7 +67,7 @@ export default function EuTournaments() {
                                 <h3 className="tour-info-other fw400">Guarantee - {data.guarantee}</h3>
                                 <h3 className="tour-info-other fw400">Invitations - {data.invite ? 'yes' : 'none'}</h3>
                                 <div className="tour-button-box">
-                                    <a href={data.link} title={data.link}>Link</a>
+                                    <a href={data.link} title={data.link}>Discord</a>
                                 </div>
                             </div>
                         </li>
@@ -58,8 +75,5 @@ export default function EuTournaments() {
                 } else {
                     return;
                 }
-            })}
-        </ol>
-    );
-
+    )
 }
