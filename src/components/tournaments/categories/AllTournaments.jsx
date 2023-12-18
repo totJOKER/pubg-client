2bbc6826b556db5959c3d292e4ad197e1646838c
totJOKER <117646766+totJOKER@users.noreply.github.com>
2023-12-18 13:53:25 +0700

    Update AllTournaments.jsx

--- src/components/tournaments/categories/AllTournaments.jsx
+++ src/components/tournaments/categories/AllTournaments.jsx
@@ -11,10 +11,29 @@ export default function AllTournaments() {
             })
     }, [])
 
+    let [curr, setCurr] = React.useState([])
+    React.useEffect(() => {
+        let data = axios.get('https://www.cbr-xml-daily.ru/latest.js')
+            .then(res => {
+                setCurr(res.data);
+            })
+    }, [])
+
+    
     let currentTour = [];
     tour.map(data => {
         currentTour.push({ id: data.id, name: data.name, orgName: data.orgName, teams: data.teams, prizepool: data.prizepool, guarantee: data.guarantee, openReg: data.openReg, link: data.link, image: data.image, currency: data.currency, invite: data.invite, regionCis: data.regionCis, regionEu: data.regionEu, closeReg: data.closeReg })
     })
+    let ratesTour = [];
+    currentTour.map(data => {
+        if (data.currency == 'rub') {
+            ratesTour.push({ id: data.id, name: data.name, orgName: data.orgName, teams: data.teams, prizepool: Math.floor(+data.prizepool * +curr.rates.USD), guarantee: data.guarantee, openReg: data.openReg, link: data.link, image: data.image, currency: 'usd', invite: data.invite, regionCis: data.regionCis, regionEu: data.regionEu, closeReg: data.closeReg })
+        } else {
+            ratesTour.push({ id: data.id, name: data.name, orgName: data.orgName, teams: data.teams, prizepool: data.prizepool, guarantee: data.guarantee, openReg: data.openReg, link: data.link, image: data.image, currency: data.currency, invite: data.invite, regionCis: data.regionCis, regionEu: data.regionEu, closeReg: data.closeReg })
+        }
+    })
+    console.log(ratesTour)
+
 
     const date = new Date();
     const year = date.getFullYear();
@@ -24,7 +43,7 @@ export default function AllTournaments() {
 
     return (
         <ol className="tour-list">
-            {currentTour.map(data => (
+            {ratesTour.map(data => (
                 <li className="tour-card show" id={data.regionCis}>
                     <div className="tour-image" style={{ backgroundImage: `url(${data.image})` }}>
                         <div className="tour-image-info">
@@ -44,9 +63,9 @@ export default function AllTournaments() {
                     <div className="tour-button-container">
                         <h3 className="tour-info-h1 fw700">{data.name}</h3>
                         <h3 className="tour-info-other fw400">Guarantee - {data.guarantee}</h3>
-                        <h3 className="tour-info-other fw400">Invitations - {data.invite ? 'yes' : 'none'}</h3>
+                        <h3 className="tour-info-other fw400">Invitations - {data.invite == '1' ? 'yes' : 'none'}</h3>
                         <div className="tour-button-box">
-                            <a href={data.link} title={data.link}>Link</a>
+                            <a href={data.link} title={data.link}>Discord</a>
                         </div>
                     </div>
                 </li>
